a31d4da952111656dd5f4d4663d4c37d4a319c6c
ssucsc <sstadler@ucsc.edu>
2024-10-11 11:33:26 -0700

    placeholder title menu

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("sdsdsdfsd");
+        StartCoroutine(LoadBossScene());
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadBossScene()
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync("BossScene");
+
+        while (!load.isDone) yield return null;
+    }
+}
